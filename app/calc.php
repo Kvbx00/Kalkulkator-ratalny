3cbb3acaf0eb04fa29cf94003524e726c32d1a23
Kvbx00 <kubapawlowski98@gmail.com>
2022-03-30 19:08:35 +0200

    xyz

--- app/calc.php
+++ app/calc.php
@@ -1,11 +1,8 @@
 <?php
 require_once dirname(__FILE__) . '/../config.php';
 
-require_once $conf->root_path .'/app/CalcCtrl.class.php';
+require_once $conf->root_path . '/app/CalcCtrl.class.php';
 // include $conf->root_path . '/app/security/check.php';
 
 $ctrl = new CalcCtrl();
 $ctrl->process();
-
-
-
